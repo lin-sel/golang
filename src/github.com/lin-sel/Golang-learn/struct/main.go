2d8f93f97dd03e3b8a8b7fbac765c63adecf70d8
lin-sel <ccnsy86@gmail.com>
2019-10-12 01:47:00 +0530

    Struct File

---
+++ src/github.com/lin-sel/Golang-learn/struct/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Student Details
+type Student struct {
+	FirstName, LastName, Dept string // Exporting Field
+	Age                       int8   // Exporting Field
+	address                   string // Non Exporting Field
+}
+
+func main() {
+
+	// Creating Student type variable
+	var p1 Student
+
+	p1 = Student{
+		"Nilesh",
+		"Yadav",
+		"CSE",
+		22,
+		"G/8 Jai Nagar Mumbai 101",
+	}
+
+	fmt.Println(p1)
+}
